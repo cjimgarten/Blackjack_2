db6621fb8813b5095b05c17f6e8500472ded5421
Chris Imgarten <cjimgarten@gmail.com>
2016-12-16 21:52:35 -0600

    modified Main.java so that dbms_username, dbms_password and db_name are passed as arguments

--- src/com/cjimgarten/controllers/main/Main.java
+++ src/com/cjimgarten/controllers/main/Main.java
@@ -2,7 +2,7 @@
  * Main.java
  * 
  * created: 10-01-2016
- * modified: 12-10-2016
+ * modified: 12-16-2016
  * 
  * launches Blackjack_2 application
  */
@@ -15,14 +15,21 @@ public class Main {
 	 * launch the application
 	 */
 	public static void main(String[] args) {
+		// ensure correct number of arguments
+		if (args.length != 3) {
+			System.out.println("Usage: java <class> <username> <password> <database>");
+			System.exit(1);
+		}
+
 		// path to the application logo
 		String logoPath = "../../images/png/jack_of_spades2.png";
 		
 		// DB credentials
-		String dbms_username = ""; /* username to access MySQL */
-		String dbms_password = ""; /* password to access MySQL */
-		String db_name = ""; /* name of the database */
+		String dbms_username = args[0]; /* username to access MySQL */
+		String dbms_password = args[1]; /* password to access MySQL */
+		String db_name = args[2]; /* name of the database */
 		
+		// initialize and start the application
 		Blackjack_2 app = new Blackjack_2(
 				"Blackjack",
 				logoPath,
